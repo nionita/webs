ce6936a756fd99a5d9338a9c22821d892bec8cda
Nicu Ionita <nicu@ionita.at>
2018-04-04 19:51:34 +0200

    Update after chapters 11 & 12, plus:
    - add batch scripts to ease local development
    - fix pagination in explore

--- app/routes.py
+++ app/routes.py
@@ -43,8 +43,8 @@ def index():
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     # Reuse the index.html template (but with no post form)
     return render_template('index.html', title='Explore', posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
---
+++ set_flask_env.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=main
+set FLASK_DEBUG=1
+set MAIL_PORT=8025
+set MAIL_SERVER=localhost
